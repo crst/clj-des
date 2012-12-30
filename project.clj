c07e8bca9dce1182a7eca54af72e60e8e115686b
chris <chris@localhost>
2012-12-30 17:34:35 +0100

    added license

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
 (defproject clj-des "0.1"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
